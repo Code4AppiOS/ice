340ff5f7c6c791a05979c8438f0d9928a1611a4e
kengsir <lishikeng@extracme.com>
2017-10-18 09:02:28 +0800

    update

---
+++ pages/shopping/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .container
+    .shopping
+      .title 王者荣耀周边
+      .list
+        .item(v-for='(item, index) in products' :key='index' @click='showProduct(item)')
+          .body
+            .title {{item.title}}
+            .content {{item.content}}
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    head() {
+      return {
+        title: '手办商城'
+      }
+    },
+    data() {
+      return {
+        
+      }
+    },
+    computed: {
+      ...mapState([
+        'houses',
+        'characters'
+        ])
+    },
+    methods: {
+    },
+    beforeCreate() {
+      this.$store.dispatch('fetchProducts')
+    }
+  }
+</script>
+
+<style scoped lang='sass' src='~static/sass/shopping.sass'></style>
+</style>
+
